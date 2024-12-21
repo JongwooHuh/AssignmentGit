569a481f4cfcbe27489d117cf0a0ddb1490fd708
82107 <82107@JwGram>
2024-12-21 16:01:16 +0900

    C.java is about your grades

--- AssignmentGit/src/git/C.java
+++ AssignmentGit/src/git/C.java
@@ -1,5 +1,8 @@
 package git;
 
 public class C {
-
+	public void Grade(String toWhom) {
+		System.out.println("Your grade is " + toWhom);
+		System.out.println("Your change in master branch");
+	}
 }
