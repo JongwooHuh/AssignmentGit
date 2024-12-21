dc689940ed13f847671ef17e1053c2698c91a0fd
82107 <82107@JwGram>
2024-12-21 16:16:56 +0900

    master branch

--- AssignmentGit/src/git/A.java
+++ AssignmentGit/src/git/A.java
@@ -5,6 +5,7 @@ public class A {
 		System.out.println("Software Engineering");
 		System.out.println("github assignment");
 		System.out.println("First Version ");
+		System.out.println("Last Version, master branch ");
 		B b = new B();
 		b.sayBye("Semester");
 		C c = new C();
