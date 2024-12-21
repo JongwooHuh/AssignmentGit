ee1d7ea94326da1f0ffa26e91cb2206c3dfd0c22
82107 <82107@JwGram>
2024-12-21 15:54:16 +0900

    Add a println lines.

--- AssignmentGit/src/git/A.java
+++ AssignmentGit/src/git/A.java
@@ -2,6 +2,9 @@ package git;
 
 public class A {
 	public static void main(String[] args) {
+		System.out.println("Software Engineering");
+		System.out.println("github assignment");
+		System.out.println("create branches");
 		
 	}
 
